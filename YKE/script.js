2724b786f055cd40cbd963133be70fb16d7d16aa
mhung2 <31676170+mhung2@users.noreply.github.com>
2017-12-16 00:32:14 -0800

    le

---
+++ YKE/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    animateDiv('#aa');
+    animateDiv('#bb');
+    animateDiv('#cc');
+    animateDiv('#dd');
+    animateDiv('#e');
+    animateDiv('#f');
+    animateDiv('#g');
+    animateDiv('#h');
+    animateDiv('#i');
+    animateDiv('#j');
+    animateDiv('#k');
+    animateDiv('#l');
+    animateDiv('#m');
+    animateDiv('#n');
+    animateDiv('#o');
+    animateDiv('#p');
+    animateDiv('#q');
+    animateDiv('#r');
+
+    // animateDiv('#igm');
+    // animateDiv('#igm2');
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    // var xspeed= 1;
+    // var yspeed=1; 
+
+    var h = $(window).height() - 100;
+    var w = $(window).width() - 100;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(myclass){
+
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+      animateDiv(myclass);        
+    });
+    
+};
